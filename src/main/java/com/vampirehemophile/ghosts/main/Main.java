cef2adf7047ae15f546a02c9886773a81c4cafc8
Antonin Décimo <antonin.decimo@gmail.com>
2015-11-16 19:59:28 +0100

    Add main package

---
+++ src/main/java/com/vampirehemophile/ghosts/main/Main.java
@@ -0,0 +1,7 @@
+package com.vampirehemophile.ghosts.main;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world ! I'm a ghost. 42.");
+  }
+}
--- src/test/java/com/vampirehemophile/ghosts/AppTest.java
+++
@@ -1,6 +0,0 @@
-package com.vampirehemophile.ghosts;
-
-import org.junit.Test;
-
-public class AppTest {
-}
