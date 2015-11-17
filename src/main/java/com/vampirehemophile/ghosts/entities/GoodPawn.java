a2ca5df2141659a78bfe45ef900604c6642fcb75
Antonin Décimo <antonin.decimo@gmail.com>
2015-11-17 16:17:09 +0100

    Add more interaction with Pawn and Player

--- src/main/java/com/vampirehemophile/ghosts/entities/GoodPawn.java
+++ src/main/java/com/vampirehemophile/ghosts/entities/GoodPawn.java
@@ -4,8 +4,14 @@ package com.vampirehemophile.ghosts.entities;
  * A good pawn that can't take opponent's pawns.
  */
 public class GoodPawn extends Pawn {
+
+  /** Constructs a new GoodPawn. */
+  public GoodPawn() {
+    super();
+  }
+
   /**
-   * Constructs a new GoodPawn.
+   * Constructs a new GoodPawn. Adds itself to the player's pawn set.
    *
    * @param player The pawn's player.
    */
@@ -15,10 +21,17 @@ public class GoodPawn extends Pawn {
 
   /**
    * Gets the pawn's char icon for CLI printing.
+   * If the pawn is not related to the player, returns a standard char.
+   * For example, if the pawn is not related to a player it will return 'G',
+   * but if it is, it may return 'x' or 'o'.
    *
    * @return pawn's char icon.
    */
   @Override public char charIcon() {
-    return player.charIcon();
+    if (player == null) {
+      return 'G';
+    } else {
+      return player.charIcon();
+    }
   }
 }
--- src/main/java/com/vampirehemophile/ghosts/entities/Player.java
+++ src/main/java/com/vampirehemophile/ghosts/entities/Player.java
@@ -2,20 +2,74 @@ package com.vampirehemophile.ghosts.entities;
 
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Spliterator;
+import java.util.Iterator;
+import java.util.function.Consumer;
 
 /**
  * Represents a player.
  */
-public class Player {
+public class Player implements Iterable<Pawn> {
+  /**
+   * Set of player's pawns.
+   */
   private Set<Pawn> pawns;
+
+  /** Player's char for CLI. */
   private char charIcon;
 
+
+  /**
+   * Constructs a new player.
+   */
   public Player(char charIcon) {
     this.pawns = new HashSet<>();
     this.charIcon = charIcon;
   }
 
+  /**
+   * Gets the player's char.
+   *
+   * @return player's char.
+   */
   public char charIcon() {
     return charIcon;
+  }
+
+  /**
+   * Add a pawn to the player's pawn set.
+   *
+   * @param p pawn to add.
+   */
+  public void add(Pawn p) {
+    if (p == null) {
+      throw new NullPointerException();
+    }
+    pawns.add(p);
+  }
+
+  /**
+   * Remove a pawn from the player's pawn set.
+   *
+   * @param p pawn to remove.
+   * @return true if the pawn set contained the specified pawn.
+   */
+  public boolean remove(Pawn p) {
+    if (p == null) {
+      throw new NullPointerException();
+    }
+    return pawns.remove(p);
+  }
+
+  @Override public void forEach(Consumer<? super Pawn> action) {
+    pawns.forEach(action);
+  }
+
+  @Override public Iterator<Pawn> iterator() {
+    return pawns.iterator();
+  }
+
+  @Override public Spliterator<Pawn> spliterator() {
+    return pawns.spliterator();
   }
 }
