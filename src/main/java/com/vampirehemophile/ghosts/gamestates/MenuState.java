e8cd1767f11e1f46652c850a0b8750f19b30f328
Antonin Décimo <antonin.decimo@gmail.com>
2015-12-13 11:07:15 +0100

    Add MenuState

--- src/main/java/com/vampirehemophile/ghosts/gamestates/MenuState.java
+++ src/main/java/com/vampirehemophile/ghosts/gamestates/MenuState.java
@@ -1,6 +1,13 @@
 package com.vampirehemophile.ghosts.gamestates;
 
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Font;
 
 /** Main menu state. */
 public class MenuState extends State {
@@ -12,6 +19,51 @@ public class MenuState extends State {
     /** Constructs a MenuPanel object. */
     public MenuPanel() {
       super();
+
+      JLabel label;
+      JButton button;
+
+      // Layout
+      setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+
+      // Title
+      label = new JLabel("Ghosts!", JLabel.CENTER);
+      label.setHorizontalTextPosition(JLabel.CENTER);
+      label.setAlignmentX(Component.CENTER_ALIGNMENT);
+      label.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 20));
+      add(label);
+
+      // Credits
+      label = new JLabel("Antonin DECIMO & Perrine PULLICINO", JLabel.CENTER);
+      label.setHorizontalTextPosition(JLabel.CENTER);
+      label.setAlignmentX(Component.CENTER_ALIGNMENT);
+      add(label);
+
+      // Play button
+      button = new JButton("Play !");
+      button.setAlignmentX(Component.CENTER_ALIGNMENT);
+      button.addActionListener(new ActionListener() {
+        @Override public void actionPerformed(ActionEvent e) {
+          MenuState.this.setChanged();
+          MenuState.this.notifyObservers(new PlayState());
+        }
+      });
+      add(button);
+
+      // Source
+      if (java.awt.Desktop.isDesktopSupported()) {
+        button = new JButton("Source");
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        button.addActionListener(new ActionListener() {
+          @Override public void actionPerformed(ActionEvent e) {
+            try {
+              java.awt.Desktop.getDesktop().browse(
+                new java.net.URI("https://github.com/VampireHemophile/Ghosts"));
+            } catch (Exception ex) {}
+          }
+        });
+        add(button);
+      }
     }
   }
 
